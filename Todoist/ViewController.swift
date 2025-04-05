bb5d3503b75021f704b2302f453dc8b4a538080a
Artem <art.kriukov@gmail.com>
2025-04-05 10:44:10 +0300

    Initial project

---
+++ Todoist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoist
+//
+//  Created by Artem Kriukov on 05.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
