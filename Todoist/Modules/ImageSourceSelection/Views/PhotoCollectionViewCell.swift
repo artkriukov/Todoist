a5370ef2f25ce390a534d548e0da1b75397702d7
Artem <art.kriukov@gmail.com>
2025-06-26 12:04:13 +0300

    TSAR-17: reset selection state in PhotoCollectionViewCell

--- Todoist/Modules/ImageSourceSelection/Views/PhotoCollectionViewCell.swift
+++ Todoist/Modules/ImageSourceSelection/Views/PhotoCollectionViewCell.swift
@@ -89,10 +89,8 @@ final class PhotoCollectionViewCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         imageView.image = nil
-//        checkmarckImageView.image = nil
-//        checkmarckContainerView.backgroundColor = nil
-//        checkmarckContainerView.layer.borderColor = nil
-//        checkmarckContainerView.layer.borderWidth = 0
+        checkmarckImageView.isHidden = true
+        checkmarckContainerView.isHidden = true
     }
 }
 
